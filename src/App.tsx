eac4e91ebf46cac4f15e9af179595e287f077381
Levi van Daalen <vandaalenlevi@gmail.com>
2024-02-26 20:12:56 +0100

    made the simple ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+type Message = {
+    owner: string;
+    message: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [history, setHistory] = useState<Message[]>([]);
+    const [generating, setGenerating] = useState<boolean>(false);
+    const [textareaCount, setTextareaCount] = useState<number>(0);
+    const [prompt, setPrompt] = useState<string>("");
+    const maxTokens = 250;
+    
+    const handleSubmit = () => {
+        const m: Message = {owner: "You", message: prompt};
+        setHistory([...history, m]);
+        setGenerating(true);
+        setPrompt("");
+        setTextareaCount(0);
+        setTimeout(() => {setHistory([...history, m, {owner: "bot", message: "Very Real Response"}]); setGenerating(false);}, 400);
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>AI Chat Bot</h1>
+            <div className="chat">
+                <div className="chat-history">
+                    {history.map(h => <p>{h.owner}: {h.message}</p>)}
+                </div>
+                <div className="textarea-wrapper">
+                    <div className="above-input">
+                        <p>{textareaCount}/{maxTokens}</p>
+                    </div>
+                    <div className="textarea-container">
+                        <textarea 
+                            disabled={generating}
+                            maxLength={maxTokens}
+                            value={prompt}
+                            onChange={e => {setTextareaCount(e.target.value.length); setPrompt(e.target.value);}}/>
+                        <button type="submit" onClick={handleSubmit}>Submit</button>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
 }
 
 export default App
